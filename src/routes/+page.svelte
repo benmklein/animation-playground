9d4d2d9c640017881307d370b19e869fd2a5d60e
Ben Klein <benmklein@gmail.com>
2024-07-14 19:17:07 -0700

    more styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,23 @@
 <script lang="ts">
-	let hover = false;
 </script>
 
 <div class="box-border h-screen bg-gradient-to-b from-gray-700 to-gray-800 p-4">
-	<div class="container m-auto h-full rounded-3xl border border-gray-800 shadow-lg shadow-gray-950">
+	<div
+		class="container m-auto h-full rounded-3xl border-2 border-gray-950 shadow-lg shadow-gray-950"
+	>
 		<div
-			class="flex h-full flex-row gap-2 rounded-3xl bg-gradient-to-br from-neutral-950 to-neutral-900"
+			class="flex h-full flex-row gap-2 rounded-3xl bg-gradient-to-t from-neutral-500 to-neutral-400"
 		>
 			<div
-				class="basis-1/6 rounded-l-3xl border-r border-r-gray-800 bg-neutral-950 p-4 text-gray-100"
+				class="basis-1/6 rounded-l-3xl border-r-2 border-r-gray-900 bg-neutral-800 p-4 text-gray-100"
 			>
-				Side bar
+				Side bar &#128512
 			</div>
 			<div class="flex basis-5/6 flex-col gap-4">
 				<div class="basis-1/6 p-4 text-gray-100">Top bar</div>
 				<div class="flex h-full flex-row gap-4 p-8">
 					<div class="flex basis-3/4 flex-col gap-4">
-						<div
-							class:hover
-							on:mouseenter={() => {
-								hover = true;
-							}}
-							on:mouseleave={() => {
-								hover = false;
-							}}
-							class="basis-1/6 rounded-xl bg-neutral-900 shadow-inner shadow-neutral-800"
-						>
-							Animating text
-						</div>
-						<div class="basis-3/6 rounded-xl bg-cyan-950 shadow-inner shadow-cyan-800"></div>
-						<div class="mb-2 basis-2/6 rounded-xl bg-cyan-950 shadow-inner shadow-cyan-800"></div>
+						<div class="text-7xl">&#129460</div>
 					</div>
 					<div
 						class="mb-2 mr-2 basis-1/4 rounded-xl bg-cyan-950 shadow-inner shadow-cyan-800"
@@ -41,7 +29,4 @@
 </div>
 
 <style lang="postcss">
-	.hover {
-		color: theme(textColor.pink.500);
-	}
 </style>
